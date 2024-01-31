14dee7e7ffe73e7825e66393a53bf4dfdc3adf3c
Nakshatra Gupta <nakshatragupta6651@gmail.com>
2024-02-01 02:07:02 +0530

    second counter project eith react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./index.css";
 import Newname from "./Newname";
 function App() {
   let [counter, setCounter] = useState(10);
@@ -12,18 +13,32 @@ function App() {
       setCounter(counter - 2);
     }
   };
-
-  if (addValue === 20) {
-  }
   return (
-    <div>
-      <h1>hello Naksh</h1>
+    <div className="container mx-auto mt-10 p-5 bg-cyan-100 rounded-lg shadow-md flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-5 bg-yellow-200">Hello Naksh</h1>
       <Newname />
-      <h1>this is New react</h1>
-      <h2>counter Value: {counter}</h2>
-      <button onClick={addValue}>Add value</button>
-      <br />
-      <button onClick={removeValue}>remove value</button>
+      <h1 className="text-xl font-semibold mb-3 bg-blue-200">
+        This is a New React App
+      </h1>
+      <h2 className="text-2xl mb-3">Counter Value: {counter}</h2>
+      <div className="flex items-center">
+        <div className="mr-3">
+          <button
+            onClick={addValue}
+            className="py-2 px-4 bg-blue-500 text-white rounded-md shadow-md"
+          >
+            Add Value
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={removeValue}
+            className="py-2 px-4 bg-red-500 text-white rounded-md shadow-md"
+          >
+            Remove Value
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Newname.js
@@ -0,0 +1,8 @@
+function Newname() {
+  return (
+    <h3 className='text-xl font-semibold mb-3bg-green-200 text-blue-500'>
+      This new function
+    </h3>
+  );
+}
+export default Newname;
